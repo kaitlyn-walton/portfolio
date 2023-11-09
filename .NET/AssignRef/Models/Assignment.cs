b1db9bbd42575a3ca0aa6a14769d62c712d1483c
Kaitlyn Walton <122939218+kaitlyn-walton@users.noreply.github.com>
2023-11-09 17:52:32 -0600

    Create Assignment.cs

---
+++ .NET/AssignRef/Models/Assignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sabio.Models.Domain;
+using Sabio.Models.Domain.Users;
+
+namespace Sabio.Models.Domain
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public LookUp AssignmentType { get; set; }
+        public LookUp3Col Position { get; set; }
+        public  User User { get; set; }
+        public decimal Fee { get; set; }
+        public LookUp AssignmentStatus { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set;}
+    }
+}

e5a3964264b76829177e5b0da9c3afa79ede8e13
Kaitlyn Walton <122939218+kaitlyn-walton@users.noreply.github.com>
2023-11-09 19:43:27 -0600

    Create CourseSubscription.cs

---
+++ .NET/Monefi/Models/CourseSubscription.cs
@@ -0,0 +1,6 @@
+ public class CourseSubscription
+    {
+        public BaseUser User { get; set; }
+        public string CourseIds { get; set; }
+        public string CourseNames { get; set; }
+    }

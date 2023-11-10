492a522e02df674ca78126263a1179245126cb03
Kaitlyn Walton <122939218+kaitlyn-walton@users.noreply.github.com>
2023-11-09 18:02:54 -0600

    Update GradeType.cs

--- .NET/AssignRef/Models/GradeType.cs
+++ .NET/AssignRef/Models/GradeType.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Domain.Grades
-{
     public class GradeType : LookUp3Col
     {        
         public bool IsReplay { get; set; }
     }
-}

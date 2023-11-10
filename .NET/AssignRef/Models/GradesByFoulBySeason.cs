e5825d624ea35b400e881d764af550a5571164de
Kaitlyn Walton <122939218+kaitlyn-walton@users.noreply.github.com>
2023-11-09 18:03:13 -0600

    Update GradesByFoulBySeason.cs

--- .NET/AssignRef/Models/GradesByFoulBySeason.cs
+++ .NET/AssignRef/Models/GradesByFoulBySeason.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Domain
-{
     public class GradesByFoulBySeason : GradesBySeason
     {
         public string Foul { get; set; }
     }
-}

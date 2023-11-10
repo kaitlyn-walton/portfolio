7aa3ec797fcdd9b1cfe9f1eedf7ee28c55b2f500
Kaitlyn Walton <122939218+kaitlyn-walton@users.noreply.github.com>
2023-11-09 18:01:41 -0600

    Update Assignment.cs

--- .NET/AssignRef/Models/Assignment.cs
+++ .NET/AssignRef/Models/Assignment.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Sabio.Models.Domain;
-using Sabio.Models.Domain.Users;
 
-namespace Sabio.Models.Domain
-{
     public class Assignment
     {
         public int Id { get; set; }
@@ -22,4 +12,3 @@ namespace Sabio.Models.Domain
         public int CreatedBy { get; set; }
         public int ModifiedBy { get; set;}
     }
-}

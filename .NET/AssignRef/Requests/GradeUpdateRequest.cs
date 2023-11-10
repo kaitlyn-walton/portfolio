ffac967200a99c4186bbc93f1fa8f69afc7abccf
Kaitlyn Walton <122939218+kaitlyn-walton@users.noreply.github.com>
2023-11-09 18:04:43 -0600

    Update GradeUpdateRequest.cs

--- .NET/AssignRef/Requests/GradeUpdateRequest.cs
+++ .NET/AssignRef/Requests/GradeUpdateRequest.cs
@@ -1,12 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests.Grades
-{
     public class GradeUpdateRequest : GradeAddRequest, IModelIdentifier
     {
         [Required]
@@ -16,4 +7,3 @@ namespace Sabio.Models.Requests.Grades
         public int StatusId { get; set; }
 
     }
-}

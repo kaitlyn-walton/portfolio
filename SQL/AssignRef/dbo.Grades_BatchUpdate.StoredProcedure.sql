ef0ab6b3d76669a02f4cf62eaa67b15203de08bf
Kaitlyn Walton <122939218+kaitlyn-walton@users.noreply.github.com>
2023-11-09 18:25:49 -0600

    Update and rename SQL/dbo.Grades_BatchUpdate.StoredProcedure.sql to SQL/AssignRef/dbo.Grades_BatchUpdate.StoredProcedure.sql

---
+++ SQL/AssignRef/dbo.Grades_BatchUpdate.StoredProcedure.sql
@@ -0,0 +1,33 @@
+
+CREATE proc [dbo].[Grades_BatchUpdate]
+			@BatchUpdateGrades dbo.BatchUpdateGrades READONLY
+			,@ModifiedBy int
+
+as
+/*
+	declare @BatchUpdateGrades dbo.BatchUpdateGrades
+				,@ModifiedBy int = 6
+
+	insert into @BatchUpdateGrades (FoulId, GradeTypeId, Comment, StatusId, Id)
+	values (), ()
+
+	execute [dbo].[Grades_BatchUpdate] 
+								@BatchUpdateGrades
+								,@ModifiedBy
+
+*/
+BEGIN
+
+	declare @dateNow datetime2(7) = getutcdate()
+
+	Update [dbo].[Grades]
+		Set [FoulId] = b.[FoulId]
+			,[GradeTypeId] = b.[GradeTypeId]
+			,[Comment] = b.[Comment]
+			,[ModifiedBy] = @ModifiedBy
+			,[DateModified] = @dateNow
+			,[StatusId] = b.[StatusId]
+	From @BatchUpdateGrades as b inner join dbo.Grades as g
+				on g.[Id] = b.[Id]
+
+END

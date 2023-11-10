3af86845221bb50c89166414d1448a0966f8cff3
Kaitlyn Walton <122939218+kaitlyn-walton@users.noreply.github.com>
2023-11-09 18:02:03 -0600

    Update FoulsBySeason.cs

--- .NET/AssignRef/Models/FoulsBySeason.cs
+++ .NET/AssignRef/Models/FoulsBySeason.cs
@@ -1,16 +1,7 @@
-﻿using Sabio.Models.Domain.Seasons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain
-{
     public class FoulsBySeason
     {
         public Season Season { get; set; }
         public int ConferenceId { get; set; }
         public int Total { get; set; }
     }
-}
